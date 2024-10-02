abc5aef6cfeb9dec1ad4762983fd5182be219a7a
George <ogomez.isc@gmail.com>
2024-10-01 19:54:30 -0600

    Creando el header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header () {
+
+
+    return(
+        <header className="bg-purple-200 py-10">
+            <div className="container mx-auto flex justify-between items-center p-4">
+                <div className="flex items-center">
+                    <img src="/img/logo.png" className="h-12"/>                
+                </div>
+            </div>
+        </header>
+         
+    )
+}
